089c9994733276bc357e6af6129a418d8a40fe71
yk Wei <ykwei7@gmail.com>
2023-09-30 20:13:38 +0800

    Add dotenv for environmental variables

--- backend/db.py
+++ backend/db.py
@@ -1,25 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
 import urllib.parse
+import os
+
 
 class Base(DeclarativeBase):
     pass
 
-user = 'htx_admin'
-password = '<redacted>'
-host = 'htx-23.mysql.database.azure.com'
-port = 3306
-database = 'htx_23'
+
+load_dotenv()
+
+
+user = os.getenv("user")
+password = os.getenv("password")
+host = os.getenv("host")
+port = os.getenv("port")
+database = os.getenv("database")
+
 
 def get_db_uri():
     uri = "mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
-        urllib.parse.quote(user), 
-        urllib.parse.quote(password), 
-        urllib.parse.quote(host), 
-        port, 
-        urllib.parse.quote(database)
+        urllib.parse.quote(user),
+        urllib.parse.quote(password),
+        urllib.parse.quote(host),
+        port,
+        urllib.parse.quote(database),
     )
     print(uri)
     return uri
+
 
 db = SQLAlchemy(model_class=Base)
